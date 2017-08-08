07304f309aa095e769ee43647eb5325e09d7f040
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-08-08 16:45:21 +0200

    Update slow.sql

--- mysql/opt/slow.sql
+++ mysql/opt/slow.sql
@@ -1,2 +1,2 @@
-SELECT *, MICROSECOND(query_time) AS 'query_time_microseconds', MICROSECOND(lock_time) AS 'lock_time_microseconds' FROM slow_log;
+SELECT *, MICROSECOND(query_time) AS 'query_time_microseconds', MICROSECOND(lock_time) AS 'lock_time_microseconds' FROM mysql.slow_log;
 TRUNCATE slow_log;
