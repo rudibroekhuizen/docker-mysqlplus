035ed84719063178b1138be0c20c559210c2d282
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-09-07 14:51:05 +0200

    Delete mysql_log.sql

--- mysql/opt/mysql_log.sql
+++
@@ -1,5 +0,0 @@
-SELECT *, "general_log" as tag FROM mysql.general_log;
-TRUNCATE general_log;
-
-SELECT *, MICROSECOND(query_time) AS 'query_time_microseconds', MICROSECOND(lock_time) AS 'lock_time_microseconds', "slow_log" as tag FROM mysql.slow_log;
-TRUNCATE slow_log;
