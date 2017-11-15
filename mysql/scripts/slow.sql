832cfe4979042496f9efe1f68ee29beb7a309efe
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-11-15 10:38:41 +0100

    Convert sql_text from blob

--- mysql/scripts/slow.sql
+++ mysql/scripts/slow.sql
@@ -1,3 +1,2 @@
-SELECT *, MICROSECOND(query_time) AS 'query_time_microseconds', MICROSECOND(lock_time) AS 'lock_time_microseconds' FROM mysql.slow_log;
-# SELECT *, SECOND(query_time) AS 'query_time_seconds', SECOND(lock_time) AS 'lock_time_seconds' FROM mysql.slow_log;
+SELECT start_time, user_host, MICROSECOND(query_time) AS 'query_time_microseconds', MICROSECOND(lock_time) AS 'lock_time_microseconds', rows_sent, rows_examined, db, last_insert_id, insert_id, server_id, CONVERT(sql_text USING utf8) as sql_text, thread_id FROM mysql.slow_log;
 TRUNCATE mysql.slow_log;
