207f12fdb94f7e0fb9d8309da31311e1310f6858
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-11-16 11:35:23 +0100

    Convert blob

--- mysql/scripts/general.sql
+++ mysql/scripts/general.sql
@@ -1,2 +1,2 @@
-SELECT * FROM mysql.general_log;
+SELECT event_time, user_host, thread_id, server_id, command_type, CONVERT(argument USING utf8) as argument FROM mysql.general_log;
 TRUNCATE mysql.general_log;
