f985b4e6da6849c0e262178a314b2666450f017c
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-01-12 14:01:31 +0100

    Abc

--- mysqlsh/scripts/sakila.sh
+++ mysqlsh/scripts/sakila.sh
@@ -3,5 +3,10 @@
 wget --quiet -O /tmp/sakila-db.tar.gz http://downloads.mysql.com/docs/sakila-db.tar.gz
 tar -zxf /tmp/sakila-db.tar.gz -C /tmp/
 
-mysql -b -N -u root -pmypass mysql < /tmp/sakila-db/sakila-schema.sql
-mysql -b -N -u root -pmypass mysql < /tmp/sakila-db/sakila-data.sql
+#mysql -b -N -u root -pmypass mysql < /tmp/sakila-db/sakila-schema.sql
+#mysql -b -N -u root -pmypass mysql < /tmp/sakila-db/sakila-data.sql
+
+mysql -b -N -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < /tmp/sakila-db/sakila-schema.sql
+mysql -b -N -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < /tmp/sakila-db/sakila-data.sql
+
+
