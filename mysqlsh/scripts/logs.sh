dfa0e0e6b12acaf9519d5bafd2968b679a11b9c0
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-03-01 14:34:46 +0100

    mysqlsh

--- mysqlsh/scripts/logs.sh
+++ mysqlsh/scripts/logs.sh
@@ -5,3 +5,5 @@ yes "mysql -b -N -h mysql -u root -pmypass mysql < /scripts/slow.sql >> /tmp/slo
 
 yes "mysql -b -N -h mysql -u root -pmypass mysql < /scripts/general.sql >> /tmp/general.log" | parallel --jobs 1 --delay 60
 #yes "mysql -b -N -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < /scripts/general.sql >> /tmp/general.log" | parallel --jobs 1 --delay 60 &
+
+#mysqlsh --uri root:mypass@mysql:3306 --schema=mysql --json=raw --sqlc --file=slow.sql
