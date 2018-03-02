93969014cd3b5f7941978f8939dd1b495d63677d
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-03-02 16:23:13 +0100

    yes yes

--- mysqlsh/scripts/logs.sh
+++ mysqlsh/scripts/logs.sh
@@ -2,18 +2,17 @@
 
 # slow_log
 yes "mysql -b -N -h mysql -u root -pmypass mysql < /scripts/slow.sql >> /tmp/slow.log" | parallel --jobs 1 --delay 60 &
-#yes "mysql -b -N -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < /scripts/slow.sql >> /tmp/slow.log" | parallel --jobs 1 --delay 60
+#yes "mysql -b -N -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < /scripts/slow.sql >> /tmp/slow.log" | parallel --jobs 1 --delay 60 &
 
 # general_log
 yes "mysql -b -N -h mysql -u root -pmypass mysql < /scripts/general.sql >> /tmp/general.log" | parallel --jobs 1 --delay 60
 #yes "mysql -b -N -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < /scripts/general.sql >> /tmp/general.log" | parallel --jobs 1 --delay 60 &
 
 # global_status (strings/numbers
-mysqlsh --json --sqlc --uri root:mypass@mysql:3306/mysql < global_status.sql | jq '.rows[] | .value|=tonumber? // .value|=tostring' | jq -c -s 'from_entries' >> /tmp/json.json
+yes "mysqlsh --json --sqlc --uri root:mypass@mysql:3306/mysql < global_status.sql | jq '.rows[] | .value|=tonumber? // .value|=tostring' | jq -c -s 'from_entries' >> /tmp/json.json" | parallel --jobs 1 --delay 60 &
 
 # global_variables (all strings: No matching token for number_type [BIG_INTEGER] in elasticsearch)
-mysqlsh --json --sqlc --uri root:mypass@mysql:3306/mysql < global_variables.sql | jq -c '.rows | from_entries' >> /tmp/json.json
-#mysqlsh --json --sqlc --uri root:mypass@mysql:3306/mysql < global_variables.sql | jq '.rows[] | .value|=tonumber? // .value|=tostring' | jq -c -s 'from_entries' >> /tmp/json.json
+yes "mysqlsh --json --sqlc --uri root:mypass@mysql:3306/mysql < global_variables.sql | jq -c '.rows | from_entries' >> /tmp/json.json" | parallel --jobs 1 --delay 60 &
 
 
 
