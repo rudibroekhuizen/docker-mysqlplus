15279339f2a6c79cf6c4ff23a211f0c9ce4612b5
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-03-02 13:38:32 +0100

    global_status to json

--- mysqlsh/scripts/logs.sh
+++ mysqlsh/scripts/logs.sh
@@ -6,5 +6,8 @@ yes "mysql -b -N -h mysql -u root -pmypass mysql < /scripts/slow.sql >> /tmp/slo
 yes "mysql -b -N -h mysql -u root -pmypass mysql < /scripts/general.sql >> /tmp/general.log" | parallel --jobs 1 --delay 60
 #yes "mysql -b -N -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < /scripts/general.sql >> /tmp/general.log" | parallel --jobs 1 --delay 60 &
 
+# global_status
+mysqlsh --json --sqlc --uri root:mypass@mysql:3306/mysql < global_status.sql | jq '.rows[] | .value|=tonumber? // .value|=tostring' | jq -c -s 'from_entries' >> /tmp/global_status.json
+
 #mysqlsh --uri root:mypass@mysql/mysql:3306 --json=raw --sqlc --file=slow.sql
 #mysqlsh --uri root:mypass@mysql:3306/mysql --classic --sqlc < general.sql 
