ee84bb65531a317b9f2e66969e043b4f6e9ee14c
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-03-02 15:02:05 +0100

    Add tag

--- mysqlsh/scripts/global_status.sql
+++ mysqlsh/scripts/global_status.sql
@@ -1,1 +1,3 @@
-SELECT VARIABLE_NAME AS 'key', VARIABLE_VALUE AS 'value' FROM performance_schema.global_status;
+SELECT VARIABLE_NAME AS 'key', VARIABLE_VALUE AS 'value' FROM performance_schema.global_status
+UNION (
+SELECT 'Tag' AS k, 'global_status' AS v);
