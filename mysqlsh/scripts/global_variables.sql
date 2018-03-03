fe75facde470a0a5bb3613aa2c1137c4adcb68d7
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-03-03 18:25:29 +0100

    Update global_variables.sql

--- mysqlsh/scripts/global_variables.sql
+++ mysqlsh/scripts/global_variables.sql
@@ -1,3 +1,3 @@
 SELECT VARIABLE_NAME AS 'key', VARIABLE_VALUE AS 'value' FROM performance_schema.global_variables
 UNION (
-SELECT 'tag' AS k, 'global_variables' AS v);
+SELECT 'Tag' AS k, 'global_variables' AS v);
