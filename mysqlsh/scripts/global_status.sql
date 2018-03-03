68ba157d135e9c442aab3b79496e8115904b076c
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-03-03 16:13:38 +0100

    Update global_status.sql

--- mysqlsh/scripts/global_status.sql
+++ mysqlsh/scripts/global_status.sql
@@ -1,3 +1,3 @@
 SELECT VARIABLE_NAME AS 'key', VARIABLE_VALUE AS 'value' FROM performance_schema.global_status
 UNION (
-SELECT 'Tag' AS k, 'global_status' AS v);
+SELECT 'tag' AS k, 'global_status' AS v);
