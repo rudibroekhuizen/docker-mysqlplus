f4c01b3cb24af8b604f1268023497617ca0b1e00
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-03-04 19:13:14 +0100

    Update logs.sh

--- mysqlsh/scripts/logs.sh
+++ mysqlsh/scripts/logs.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# global_status (strings/numbers
+yes "mysqlsh --json --sqlc --uri ${MYSQL_ANALYTICS_USER}:${MYSQL_ANALYTICS_PASSWORD}@mysql:3306/mysql < global_status.sql | jq '.rows[] | .value|=tonumber? // .value|=tostring' | jq -c -s 'from_entries' >> /tmp/json.json" | parallel --jobs 1 --delay 60 &
+
+# global_variables (all strings: No matching token for number_type [BIG_INTEGER] in elasticsearch)
+yes "mysqlsh --json --sqlc --uri ${MYSQL_ANALYTICS_USER}:${MYSQL_ANALYTICS_PASSWORD}@mysql:3306/mysql < global_variables.sql | jq -c '.rows | from_entries' >> /tmp/json.json" | parallel --jobs 1 --delay 60 &
+
 # slow_log
 yes "mysql -b -N -h mysql -u root -pmypass mysql < /scripts/slow.sql >> /tmp/slow.log" | parallel --jobs 1 --delay 60 &
 #yes "mysql -b -N -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < /scripts/slow.sql >> /tmp/slow.log" | parallel --jobs 1 --delay 60 &
@@ -7,12 +13,6 @@ yes "mysql -b -N -h mysql -u root -pmypass mysql < /scripts/slow.sql >> /tmp/slo
 # general_log
 yes "mysql -b -N -h mysql -u root -pmypass mysql < /scripts/general.sql >> /tmp/general.log" | parallel --jobs 1 --delay 60 &
 #yes "mysql -b -N -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < /scripts/general.sql >> /tmp/general.log" | parallel --jobs 1 --delay 60 &
-
-# global_status (strings/numbers
-yes "mysqlsh --json --sqlc --uri ${MYSQL_ANALYTICS_USER}:${MYSQL_ANALYTICS_PASSWORD}@mysql:3306/mysql < global_status.sql | jq '.rows[] | .value|=tonumber? // .value|=tostring' | jq -c -s 'from_entries' >> /tmp/json.json" | parallel --jobs 1 --delay 60 &
-
-# global_variables (all strings: No matching token for number_type [BIG_INTEGER] in elasticsearch)
-yes "mysqlsh --json --sqlc --uri ${MYSQL_ANALYTICS_USER}:${MYSQL_ANALYTICS_PASSWORD}@mysql:3306/mysql < global_variables.sql | jq -c '.rows | from_entries' >> /tmp/json.json" | parallel --jobs 1 --delay 60 &
 
 
 
