d68a5c95e2c8a27d92c9dc0703c6b1adb0e79b24
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-03-05 21:08:43 +0100

    Update logs.sh

--- mysqlsh/scripts/logs.sh
+++ mysqlsh/scripts/logs.sh
@@ -7,12 +7,12 @@ yes "mysqlsh --json --sqlc --uri ${MYSQL_ANALYTICS_USER}:${MYSQL_ANALYTICS_PASSW
 yes "mysqlsh --json --sqlc --uri ${MYSQL_ANALYTICS_USER}:${MYSQL_ANALYTICS_PASSWORD}@${MYSQL_HOST}:3306/mysql < /scripts/global_variables.sql | jq -c '.rows | from_entries' >> /tmp/json.json" | parallel --jobs 1 --delay 60 &
 
 # slow_log
-yes "mysql -b -N -h mysql -u root -pmypass mysql < /scripts/slow.sql >> /tmp/slow.log" | parallel --jobs 1 --delay 60 &
-#yes "mysql -b -N -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < /scripts/slow.sql >> /tmp/slow.log" | parallel --jobs 1 --delay 60 &
+#yes "mysql -b -N -h mysql -u root -pmypass mysql < /scripts/slow.sql >> /tmp/slow.log" | parallel --jobs 1 --delay 60 &
+yes "mysql -b -N -h ${MYSQL_HOST} -u ${MYSQL_ANALYTICS_USER} -p${MYSQL_ANALYTICS_PASSWORD} mysql < /scripts/slow.sql >> /tmp/slow.log" | parallel --jobs 1 --delay 60 &
 
 # general_log
-yes "mysql -b -N -h mysql -u root -pmypass mysql < /scripts/general.sql >> /tmp/general.log" | parallel --jobs 1 --delay 60
-#yes "mysql -b -N -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < /scripts/general.sql >> /tmp/general.log" | parallel --jobs 1 --delay 60 &
+#yes "mysql -b -N -h mysql -u root -pmypass mysql < /scripts/general.sql >> /tmp/general.log" | parallel --jobs 1 --delay 60
+yes "mysql -b -N -h ${MYSQL_HOST} -u ${MYSQL_ANALYTICS_USER} -p${MYSQL_ANALYTICS_PASSWORD} mysql < /scripts/general.sql >> /tmp/general.log" | parallel --jobs 1 --delay 60 &
 
 
 
