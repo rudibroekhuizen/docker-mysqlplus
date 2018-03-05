cedef7863350f08fa632d2c2f5bfff4a9d530219
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-03-05 22:05:34 +0100

    Update create_users.sql

--- mysql/scripts/create_users.sql
+++ mysql/scripts/create_users.sql
@@ -1,4 +1,4 @@
-#mysql -b -N -u root -pmypass mysql
+#mysql -b -N -u root -pmypass mysql < /scripts/create_users.sql
 
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'mypass';
 GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION;
