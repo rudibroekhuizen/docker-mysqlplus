8d4fbcd08e6cd617e88c70f43e9179fb83aa997f
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-03-08 14:01:57 +0100

    5.5 information_schema

--- mysqlsh/scripts/global_variables.sql
+++ mysqlsh/scripts/global_variables.sql
@@ -1,3 +1,4 @@
+--SELECT LCASE(VARIABLE_NAME) AS 'key', VARIABLE_VALUE AS 'value' FROM information_schema.global_status
 SELECT LCASE(VARIABLE_NAME) AS 'key', VARIABLE_VALUE AS 'value' FROM performance_schema.global_variables
-UNION (
-SELECT 'tag' AS k, 'global_variables' AS v);
+UNION
+SELECT 'tag' AS k, 'global_variables' AS v;
