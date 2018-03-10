1f08850d8b001de30699d414880ed69404b4b396
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-03-10 15:15:34 +0100

    Update global_variables.sql

--- mysqlsh/scripts/global_variables.sql
+++ mysqlsh/scripts/global_variables.sql
@@ -1,4 +1,3 @@
---SELECT LCASE(VARIABLE_NAME) AS 'key', VARIABLE_VALUE AS 'value' FROM information_schema.global_variables
 SELECT LCASE(VARIABLE_NAME) AS 'key', VARIABLE_VALUE AS 'value' FROM performance_schema.global_variables
 UNION
 SELECT 'tag' AS k, 'global_variables' AS v;
