8ac78524a4d0c03c0532b0f1e5c8b1d2a8bd3fcf
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-03-10 20:07:50 +0100

    Update db_size.sql

--- mysqlsh/scripts/db_size.sql
+++ mysqlsh/scripts/db_size.sql
@@ -4,9 +4,3 @@ SELECT 'db_size' as tag, table_schema "database name",
 FROM information_schema.TABLES
 WHERE table_schema not in ('information_schema','mysql','performance_schema','sys')
 GROUP BY table_schema;
-
--- Top 10 table size
---SELECT CONCAT(table_schema, '.', table_name) AS 'key',
---(data_length + index_length) as 'value' FROM information_schema.tables
---WHERE table_schema not in ('information_schema','mysql','performance_schema','sys') 
---ORDER BY value DESC LIMIT 10;
