3332cf157339134b42532b65c0661b9981dbd242
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-03-10 11:04:19 +0100

    Update tables.sql

--- mysqlsh/scripts/tables.sql
+++ mysqlsh/scripts/tables.sql
@@ -1,4 +1,13 @@
-SELECT CONCAT(table_schema, '.', table_name) AS 'key',
-(data_length + index_length) as 'value' FROM information_schema.tables
-WHERE table_schema not in ('information_schema','mysql','performance_schema','sys') 
-ORDER BY value DESC LIMIT 10;
+-- Database size and free space
+SELECT table_schema "database name",
+  sum( data_length + index_length ) AS "database_size",
+  sum( data_free ) AS "free_space"
+FROM information_schema.TABLES
+WHERE table_schema not in ('information_schema','mysql','performance_schema','sys')
+GROUP BY table_schema;
+
+-- Top 10 table size
+--SELECT CONCAT(table_schema, '.', table_name) AS 'key',
+--(data_length + index_length) as 'value' FROM information_schema.tables
+--WHERE table_schema not in ('information_schema','mysql','performance_schema','sys') 
+--ORDER BY value DESC LIMIT 10;
