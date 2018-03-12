6b96bfe50805ede14e36f44766c7768bbc169acc
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-03-12 15:41:51 +0100

    /*!50706 <code> */

--- mysqlsh/scripts/global_status.sql
+++ mysqlsh/scripts/global_status.sql
@@ -1,3 +1,10 @@
+/*MySQL 5.7.6 and up only*/
+/*!50706
 SELECT LCASE(VARIABLE_NAME) AS 'key', VARIABLE_VALUE AS 'value' FROM performance_schema.global_status
+UNION
+SELECT 'tag' AS k, 'global_status' AS v
+*/;
+
+SELECT LCASE(VARIABLE_NAME) AS 'key', VARIABLE_VALUE AS 'value' FROM information_schema.global_status
 UNION
 SELECT 'tag' AS k, 'global_status' AS v;
