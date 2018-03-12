f32d341d445ddab3d676c5c4bdb932c0154f8f14
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-03-12 15:46:25 +0100

    Continue if error - force

--- mysqlsh/scripts/logs.sh
+++ mysqlsh/scripts/logs.sh
@@ -3,19 +3,19 @@
 # last line without & character!
 
 # database size and free space
-yes "mysqlsh --json --sqlc --uri ${MYSQL_ANALYTICS_USER}:${MYSQL_ANALYTICS_PASSWORD}@${MYSQL_HOST}:3306/mysql < /scripts/db_size.sql | jq -c '.rows[] | .database_size|=tonumber | .free_space|=tonumber' >> /tmp/json.json" | parallel --jobs 1 --delay 60 &
+yes "mysqlsh --force --json --sqlc --uri ${MYSQL_ANALYTICS_USER}:${MYSQL_ANALYTICS_PASSWORD}@${MYSQL_HOST}:3306/mysql < /scripts/db_size.sql | jq -c '.rows[] | .database_size|=tonumber | .free_space|=tonumber' >> /tmp/json.json" | parallel --jobs 1 --delay 60 &
 
 # global_status (strings/numbers
-yes "mysqlsh --json --sqlc --uri ${MYSQL_ANALYTICS_USER}:${MYSQL_ANALYTICS_PASSWORD}@${MYSQL_HOST}:3306/mysql < /scripts/global_status.sql | jq '.rows[] | .value|=tonumber? // .value|=tostring' | jq -c -s 'from_entries' >> /tmp/json.json" | parallel --jobs 1 --delay 60 &
+yes "mysqlsh --force --json --sqlc --uri ${MYSQL_ANALYTICS_USER}:${MYSQL_ANALYTICS_PASSWORD}@${MYSQL_HOST}:3306/mysql < /scripts/global_status.sql | jq '.rows[] | .value|=tonumber? // .value|=tostring' | jq -c -s 'from_entries' >> /tmp/json.json" | parallel --jobs 1 --delay 60 &
 
 # global_variables (all strings: No matching token for number_type [BIG_INTEGER] in elasticsearch)
-yes "mysqlsh --json --sqlc --uri ${MYSQL_ANALYTICS_USER}:${MYSQL_ANALYTICS_PASSWORD}@${MYSQL_HOST}:3306/mysql < /scripts/global_variables.sql | jq -c '.rows | from_entries' >> /tmp/json.json" | parallel --jobs 1 --delay 60 &
+yes "mysqlsh --force --json --sqlc --uri ${MYSQL_ANALYTICS_USER}:${MYSQL_ANALYTICS_PASSWORD}@${MYSQL_HOST}:3306/mysql < /scripts/global_variables.sql | jq -c '.rows | from_entries' >> /tmp/json.json" | parallel --jobs 1 --delay 60 &
 
 # slow_log
-yes "mysql -b -N -h ${MYSQL_HOST} -u ${MYSQL_ANALYTICS_USER} -p${MYSQL_ANALYTICS_PASSWORD} mysql < /scripts/slow.sql >> /tmp/slow.log" | parallel --jobs 1 --delay 60 &
+yes "mysql -f -b -N -h ${MYSQL_HOST} -u ${MYSQL_ANALYTICS_USER} -p${MYSQL_ANALYTICS_PASSWORD} mysql < /scripts/slow.sql >> /tmp/slow.log" | parallel --jobs 1 --delay 60 &
 
 # general_log
-yes "mysql -b -N -h ${MYSQL_HOST} -u ${MYSQL_ANALYTICS_USER} -p${MYSQL_ANALYTICS_PASSWORD} mysql < /scripts/general.sql >> /tmp/general.log" | parallel --jobs 1 --delay 60
+yes "mysql -f -b -N -h ${MYSQL_HOST} -u ${MYSQL_ANALYTICS_USER} -p${MYSQL_ANALYTICS_PASSWORD} mysql < /scripts/general.sql >> /tmp/general.log" | parallel --jobs 1 --delay 60
 
 
 
