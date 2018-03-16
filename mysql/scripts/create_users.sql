c5b856bf930686516784873eb6a302bef203ef8b
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-03-16 16:04:52 +0100

    Update create_users.sql

--- mysql/scripts/create_users.sql
+++ mysql/scripts/create_users.sql
@@ -1,10 +1,18 @@
 --mysql -b -N -u root -pmypass mysql < /scripts/create_users.sql
 
+FLUSH PRIVILEGES;
+
+ALTER USER 'analytics'@'localhost' IDENTIFIED BY 'mypass';
+ALTER USER 'analytics'@'%' IDENTIFIED BY 'mypass';
+
 CREATE USER 'analytics'@'localhost' IDENTIFIED BY 'mypass';
-GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'analytics'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'analytics'@'localhost';
 
 CREATE USER 'analytics'@'%' IDENTIFIED BY 'mypass';
-GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'analytics'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'analytics'@'%';
 
---CREATE USER 'admin'@'localhost' IDENTIFIED BY 'mypass';
---GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION;
+CREATE USER 'admin'@'localhost' IDENTIFIED BY 'mypass';
+GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost';
+
+CREATE USER 'admin'@'%' IDENTIFIED BY 'mypass';
+GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%';
