1c071d12d218751ba3668014d10dba2d37ebb977
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-04-12 20:46:31 +0200

    Add hostname in global_status query

--- mysqlsh/scripts/global_status.sql
+++ mysqlsh/scripts/global_status.sql
@@ -2,6 +2,8 @@
 /*!50706
 SELECT LCASE(VARIABLE_NAME) AS 'key', VARIABLE_VALUE AS 'value' FROM performance_schema.global_status
 UNION
+SELECT 'hostname' AS k, @@hostname AS v
+UNION
 SELECT 'tag' AS k, 'global_status' AS v
 */;
 
